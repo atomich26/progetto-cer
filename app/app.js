af1072fb0d41f2bef443ed5f3152552b0e7cb968
atomich26 <michele.bev@outlook.com>
2024-06-17 18:31:51 +0200

    Aggiunta app node

--- app/app.js
+++ app/app.js
@@ -1,9 +1,20 @@
-import express from "express";
+const express = require("express");
 const APP = express();
-const PORT = process.env.port;
+const PORT = 3000;
+
+// Imposta il template engine HTML Pug
+APP.set('view engine', 'pug');
 
 APP.get('/', (req, res) => {
-    res.send("Ciao mondo!");
+    res.render("index", { title: "Soundwave", message: "Benvenuto in Soundwave " });
+});
+
+APP.get("/about", (req, res) => {
+    res.send("About page");
+});
+
+APP.get("/policy", (req, res) => {
+    res.send("Policy page");
 });
 
 APP.listen(PORT, () => {
